ae245240dd41a6b34f0f451ac5fd1932b8de56b8
Dell <Dell@DESKTOP-UPGG4QD.domain.name>
2023-03-16 17:20:30 +0530

    First Commit

---
+++ c2tc_vvit/src/com/tnsif/programs/Car.java
@@ -0,0 +1,16 @@
+package com.tnsif.programs;
+
+public class Car {
+	int model;
+	String color;
+	int registration;
+	void forward() {
+		System.out.println("Car is moving forward");
+	}
+    void backward() {
+    	System.out.println("Car is moving backward");
+    }
+    void stop() {
+    	System.out.println("Car has stopped");
+    }
+}
---
+++ c2tc_vvit/src/com/tnsif/programs/Executor.java
@@ -0,0 +1,16 @@
+package com.tnsif.programs;
+
+import java.util.Scanner;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car ob=new Car();
+ob.backward();
+ob.forward();
+ob.stop();
+Scanner sc=new Scanner(System.in);
+	}
+
+}

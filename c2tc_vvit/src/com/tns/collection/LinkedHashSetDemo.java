b410526d9e45f936d61504fdf99ad4fcf48782a1
Dell <Dell@DESKTOP-UPGG4QD.domain.name>
2023-04-11 16:13:44 +0530

    commit

---
+++ c2tc_vvit/src/com/tns/collection/LinkedHashSetDemo.java
@@ -0,0 +1,19 @@
+package com.tns.collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+	
+	// TODO Auto-generated method stub
+   LinkedHashSet<Integer> l=new LinkedHashSet<Integer>();	{
+	   l.add(1);
+	   l.add(2);
+	   l.add(5);
+	   l.add(0);
+	   System.out.println(l);
+   }
+   }
+
+}
